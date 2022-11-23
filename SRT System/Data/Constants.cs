1da33663bb83794a1e33da030a409d565e3215a4
MbongX-D <Mbongenimahlangu203@outlook.com>
2022-11-23 21:12:11 +0200

    Constants update

--- SRT System/Data/Constants.cs
+++ SRT System/Data/Constants.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.CodeDom;
 using System.Collections.Generic;
+using System.DirectoryServices.ActiveDirectory;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,14 @@ namespace SRT_System.Data
 {
     internal class Constants
     {
-        public static string jj;
+        public const String SOURCE = "srts.database.windows.net";
+        public const String ID = "MbongXD";
+        public const String KEYS = "Magwegwe203@";
+        public const String DATABASE_NAME = "DevTestsRevamped";
+        public const bool ENCRYPT_STATUS = true;
+        public const bool TRUST_SERVER_CERTIFICATE = false;
+        public const String H_NAME_CERTIFICATE = "*.database.windows.net";
+        public const Int32 CON_TIMEOUT = 30;
+
     }
 }
--- SRT System/Data/DCon.cs
+++ SRT System/Data/DCon.cs
@@ -1,35 +1,39 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using System.Reflection.Metadata;
+using Microsoft.Data.SqlClient;
 
 namespace SRT_System.Data 
 {
     internal class DCon
     {
         public DCon() 
-        {
-
-                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
-                builder.DataSource = "srts.database.windows.net";
-                builder.UserID = "MbongXD";
-                builder.Password = "Magwegwe203@";
-                builder.InitialCatalog = "DevTestsRevamped";
-                builder.Encrypt = true;
-                builder.TrustServerCertificate = false;
-                builder.HostNameInCertificate = "*.database.windows.net";
-                builder.ConnectTimeout = 30;
-
-
-            
-            //Console.ReadLine();
+        { //Console.ReadLine();
         }
+        
 
         public void PerformLogin(string u,string p)
         {
-            
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = Constants.SOURCE;
+            builder.UserID = Constants.ID;
+            builder.Password = Constants.KEYS;
+            builder.InitialCatalog = Constants.DATABASE_NAME;
+            builder.Encrypt = Constants.ENCRYPT_STATUS;
+            builder.TrustServerCertificate = Constants.TRUST_SERVER_CERTIFICATE;
+            builder.HostNameInCertificate = Constants.H_NAME_CERTIFICATE;
+            builder.ConnectTimeout = Constants.CON_TIMEOUT;
         }
 
         public void PerformRegistration()
         {
-            
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = Constants.SOURCE;
+            builder.UserID = Constants.ID;
+            builder.Password = Constants.KEYS;
+            builder.InitialCatalog = Constants.DATABASE_NAME;
+            builder.Encrypt = Constants.ENCRYPT_STATUS;
+            builder.TrustServerCertificate = Constants.TRUST_SERVER_CERTIFICATE;
+            builder.HostNameInCertificate = Constants.H_NAME_CERTIFICATE;
+            builder.ConnectTimeout = Constants.CON_TIMEOUT;
         }
         //crud methods
         
