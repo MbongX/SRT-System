a21c5bd4015f5634dff7ac30d7ec001bacf5eff2
MbongX <mbongenimahlangu203@outlook.com>
2023-01-22 21:51:26 +0200

    update

--- SRT System/Data/Constants.cs
+++ SRT System/Data/Constants.cs
@@ -11,15 +11,15 @@ namespace SRT_System.Data
     internal class Constants
     {
         public const String SOURCE = "srts.database.windows.net";
-        public const String ID = "MbongXD";
-        public const String KEYS = "Magwegwe203@";
+        public const String ID = "";
+        public const String KEYS = "";
         public const String DATABASE_NAME = "tests";
         public const bool ENCRYPT_STATUS = true;
         public const bool TRUST_SERVER_CERTIFICATE = false;
         public const String H_NAME_CERTIFICATE = "*.database.windows.net";
         public const Int32 CON_TIMEOUT = 30;
 
-        public const String CON_STRING = "Server=tcp:srts.database.windows.net,1433;Initial Catalog=tests;Persist Security Info=False;User ID=MbongXD;Password=Magwegwe203@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        public const String CON_STRING = "Server=tcp:srts.database.windows.net,1433;Initial Catalog=tests;Persist Security Info=False;User ID=;Password=;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
     }
 }
