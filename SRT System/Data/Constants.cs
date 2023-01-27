1d4686aa8b2675e0c5843e5ab134537b672ec610
MbongX <mbongenimahlangu203@outlook.com>
2023-01-27 12:12:53 +0200

    Update Constants.cs

--- SRT System/Data/Constants.cs
+++ SRT System/Data/Constants.cs
@@ -16,12 +16,7 @@ namespace SRT_System.Data
         public const String DATABASE_NAME = "Student Review And Tutoring System";
         public const bool ENCRYPT_STATUS = true;
         public const bool TRUST_SERVER_CERTIFICATE = false;
-        public const String H_NAME_CERTIFICATE = "*.database.windows.net";
+        public const String H_NAME_CERTIFICATE = "";
         public const Int32 CON_TIMEOUT = 30;
-
-        public const String CON_STRING_REG = "Server=tcp:srts.database.windows.net,1433;Initial Catalog=Student Review And Tutoring System;Persist Security Info=False;User ID=;Password=;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-        public const String CON_STRING_LOGIN = "Server=tcp:srts.database.windows.net,1433;Initial Catalog=Student Review And Tutoring System;Persist Security Info=False;User ID=;Password=;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-        public const String CON_STRING_HOME = "Server=tcp:srts.database.windows.net,1433;Initial Catalog=Student Review And Tutoring System;Persist Security Info=False;User ID=;Password=;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-        public const String CON_STRING_TS = "Server=tcp:srts.database.windows.net,1433;Initial Catalog=Student Review And Tutoring System;Persist Security Info=False;User ID=;Password=;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
     }
 }
